f36596f29032d97fc1776b4e8c8e7a34fba008df
G984162019 <198416@st.takushoku-u.ac.jp>
2025-11-19 14:46:14 +0900

    スライド16まで

--- ap08L1.js
+++ ap08L1.js
@@ -19,6 +19,8 @@ let course;
 export const origin = new THREE.Vector3();
 export const controlPoints = [
     [ 25, 40],
+    [5, 0],
+    [-20, 20],
     [-50,-20]
 ]
 export function init(scene, size, id, offset, texture) {
@@ -46,13 +48,41 @@ export function init(scene, size, id, offset, texture) {
     scene.add(plane);
 
     // ビル
+    // bldg.position.set(xx, +height/2, zz);
 
     // コース(描画)
-
+    // 制御点を保管して曲線を作る
+    course = new THREE.CatmullRomCurve3(
+        controlPoints.map((p) => {
+            return (new THREE.Vector3()).set(
+                offset.x + p[0],
+                0,
+                offset.z + p[1]
+            );
+        }), false
+    )
+    // 曲線から100箇所を取り出し、円を並べる
+    const points = course.getPoints(100);
+    points.forEach((point) => {
+        const road = new THREE.Mesh(
+            new THREE.CircleGeometry(5, 16),
+            new THREE.MeshLambertMaterial({
+                color: "gray",
+            })
+        )
+        road.rotateX(-Math.PI/2);
+        road.position.set(
+            point.x,
+            0,
+            point.z
+        );
+        scene.add(road);
+    });
 }
 
 // コース(自動運転用)
 export function makeCourse(scene) {
+    // [L1, L2, L3, L4];
 }
 
 // カメラを返す
@@ -62,6 +92,10 @@ export function getCamera() {
 
 // 車の設定
 export function setCar(scene, car) {
+    const SCALE = 0.01;
+    car.position.copy(origin);
+    car.scale.set(SCALE, SCALE, SCALE);
+    scene.add(car);
 }
 
 // Windowサイズの変更処理
