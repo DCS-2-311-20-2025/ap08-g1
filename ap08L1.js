7ed42992560312ea416e3ea1e2db5bcf1184a882
G984162019 <198416@st.takushoku-u.ac.jp>
2025-11-19 15:07:09 +0900

    スライド20まで終了

--- ap08L1.js
+++ ap08L1.js
@@ -48,7 +48,38 @@ export function init(scene, size, id, offset, texture) {
     scene.add(plane);
 
     // ビル
-    // bldg.position.set(xx, +height/2, zz);
+    function makeBuilding(x, z, type) {
+        const height = [2, 2, 7, 4, 5];
+        const bldgH = height[type] * 5;
+        const geometry = new THREE.BoxGeometry(8, bldgH, 8);
+        const material = new THREE.MeshLambertMaterial({color: 0x808080});
+        const sideUvS = (type*2+1)/11;
+        const sideUvE = (type*2+2)/11;
+        const topUvS = (type*2+2)/11;
+        const topUvE = (type*2+3)/11;
+        const uvs = geometry.getAttribute("uv");
+        for (let i = 0; i < 48; i+=4) {
+            if (i < 16 || i > 22) {
+                uvs.array[i] = sideUvS;
+                uvs.array[i+2] = sideUvE;
+            }
+            else {
+                uvs.array[i] = topUvS;
+                uvs.array[i+2] = topUvE;
+            }
+        }
+        const bldg = new THREE.Mesh(
+            geometry,
+            material
+        )
+        bldg.position.set(x, bldgH/2, z);
+        scene.add(bldg);
+    }
+    makeBuilding(20, 20, 0);
+    makeBuilding(30, 20, 1);
+    makeBuilding(40, 20, 2);
+    makeBuilding(50, 20, 3);
+    makeBuilding(60, 20, 4);
 
     // コース(描画)
     // 制御点を保管して曲線を作る
@@ -82,7 +113,22 @@ export function init(scene, size, id, offset, texture) {
 
 // コース(自動運転用)
 export function makeCourse(scene) {
-    // [L1, L2, L3, L4];
+    const courseVectors = [];
+    const parts = [L1, L2, L3, L4];
+    parts.forEach((part) => {
+        part.controlPoints.forEach((p) => {
+            courseVectors.push(
+                new THREE.Vector3(
+                    p[0] + part.origin.x,
+                    0,
+                    p[1] + part.origin.z
+                )
+            )
+        });
+    })
+    course = new THREE.CatmullRomCurve3(
+        courseVectors, false
+    )    
 }
 
 // カメラを返す
@@ -106,7 +152,15 @@ export function resize() {
 }
 
 // 描画処理
+const clock = new THREE.Clock();
+const carPosition = new THREE.Vector3();
+const carTarget = new THREE.Vector3();
 export function render(scene, car) {
+    const time = (clock.getElapsedTime() / 20);
+    course.getPointAt(time % 1, carPosition);
+    car.position.copy(carPosition);
+    course.getPointAt((time + 0.01) % 1, carTarget);
+    car.lookAt(carTarget);
     camera.lookAt(car.position.x, car.position.y, car.position.z);
     renderer.render(scene, camera);
 }
